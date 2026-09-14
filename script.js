2c54ee2efd85b53ea271b0d4a270d4981d1935de
Tamara <29234416+tam92@users.noreply.github.com>
2026-09-14 18:56:11 +0200

    Add the logic for basic math operations (functions operate, add, subtrack, multiply, and divide).

---
+++ script.js
@@ -0,0 +1,40 @@
+// -----------------------------------------------------------------------------
+// VARIABLES
+// -----------------------------------------------------------------------------
+const num1 = undefined;
+const num2 = undefined;
+const operator = undefined;
+
+
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+
+        default:
+            break;
+    }
+}
+
+// -----------------------------------------------------------------------------
+// BASIC MATH FUNCTIONS
+// -----------------------------------------------------------------------------
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
